4ed1f9c54028419951789c85e198d2cd81861758
Ayush-tech123 <ayush.sharma@avantika.edu.in>
2026-03-19 14:09:24 +0000

    Finished testing integer types

--- identity_Ayush.py
+++ identity_Ayush.py
@@ -8,3 +8,22 @@ x = 20
 print(id(x))
 print("Address of x changed")
 
+x = 25
+print(id(x))
+x += 1
+print(id(x))
+print("Address of x changed")
+
+x = 30
+print(id(x))
+x *= 2
+print(id(x))
+print("Address of x changed")
+
+x = 50
+print(id(x))
+y = x
+print(id(y))
+print("Both x and y have same address")
+
+#Hence we can see integer is an immutable data type because any change leads to new id
