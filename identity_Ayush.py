4df2281cb84385fbe952445fecf6e78f05811ec2
Ayush-tech123 <ayush.sharma@avantika.edu.in>
2026-03-19 14:30:35 +0000

    Version 2 tested float

--- identity_Ayush.py
+++ identity_Ayush.py
@@ -6,24 +6,46 @@ x = 10
 print(id(x))
 x = 20
 print(id(x))
-print("Address of x changed")
+print("Id of x changed")
 
 x = 25
 print(id(x))
 x += 1
 print(id(x))
-print("Address of x changed")
+print("Id of x changed")
 
 x = 30
 print(id(x))
 x *= 2
 print(id(x))
-print("Address of x changed")
+print("Id of x changed")
 
 x = 50
 print(id(x))
 y = x
 print(id(y))
-print("Both x and y have same address")
+print("Both x and y have same Id")
 
 #Hence we can see integer is an immutable data type because any change leads to new id
+
+#Float 
+
+x = 1.5
+print(id(x))
+x = 2.4
+print(id(x))
+print("Id of x changed")
+
+x = 3.6
+print(id(x))
+x -= 1
+print(id(x))
+print("Id of x chnaged")
+
+x = 4.2
+print(id(x))
+x = round(x,2)
+print(id(x))
+print("Id of x changed")
+
+#Hence we can see float is an immutable data type as id changes with operations on it.
